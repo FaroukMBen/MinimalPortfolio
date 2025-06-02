197bbb848ac831a6045e49c9eba3796136a1bc48
FaroukMBen <FaroukMohamed.Bendeddouche@outlook.com>
2025-06-02 16:35:53 +0200

    chore(structure): set up project structure with base components and initial data files

---
+++ src/data/ExperienceData.tsx
@@ -0,0 +1,82 @@
+export default [
+    {
+        id: 1,
+        en: {
+            Time: "Today",
+            Title: "Junior Full Stack Developer",
+            Period: "2 months",
+            Description:
+                "I was hired as a full stack developer to help build new websites and optimize existing ones. My work primarily involved PHP, JavaScript, HTML, CSS, and WordPress. I participated in everything from planning and occasional design to solving real-world problems through code.",
+        },
+        fr: {
+            Time: "Aujourd'hui",
+            Title: "Développeur Full Stack Junior",
+            Period: "2 mois",
+            Description:
+                "J’ai été recruté en tant que développeur full stack pour aider à créer de nouveaux sites web et à en optimiser d’autres. Mon travail s’est principalement appuyé sur PHP, JavaScript, HTML, CSS et WordPress. J’ai participé à toutes les étapes, de la planification à la résolution de problèmes concrets par le code, en passant parfois par la conception.",
+        },
+        Company: "BlurryAds",
+        CompanyLink: "https://blurryads.com",
+        Location: "Lyon, France",
+    },
+    {
+        id: 2,
+        en: {
+            Time: "Today",
+            Title: "Bachelor’s Degree in Computer Science",
+            Period: "2 year",
+            Description:
+                "I’m currently enrolled in a three-year Bachelor’s degree in Computer Science (BUT Informatique) at IUT Lyon 1, specializing in Application Development. I’ve completed the first two years, where I got to dive into the full software development lifecycle—from understanding needs to designing, coding, testing, and maintaining applications. The curriculum puts a strong focus on practical skills through hands-on projects, with over 600 hours of project work planned throughout the program. This experience is helping me build solid skills in programming, web development, databases, and project management, preparing me to tackle real-world problems and work efficiently in IT environments. I hope to do even better next year!",
+        },
+        fr: {
+            Time: "Aujourd'hui",
+            Title: "BUT Informatique à l'IUT Lyon 1",
+            Period: "2 ans",
+            Description:
+                "Je suis actuellement en deuxième année d’un BUT Informatique (Bachelor Universitaire de Technologie) à l’IUT Lyon 1, parcours Réalisation d’Applications. J’y ai appris à maîtriser tout le cycle de développement logiciel : comprendre les besoins, concevoir, coder, tester et maintenir des applications. La formation met l’accent sur la pratique avec des projets concrets, et prévoit plus de 600 heures de travail en mode projet sur l’ensemble du cursus. Cette expérience me permet de développer des compétences solides en programmation, développement web, bases de données et gestion de projet, pour être prêt à affronter des situations réelles dans un environnement informatique. J’espère faire encore mieux l’année prochaine !",
+        },
+        Company: "Claude Bernard University, IUT Lyon 1",
+        CompanyLink: "https://iut.univ-lyon1.fr",
+        Location: "Lyon, France",
+    },
+    {
+        id: 3,
+        en: {
+            Time: "2019",
+            Title: "DCS (Digital and Computer Science), High School",
+            Period: "3 year",
+            Description:
+                "I completed my high school diploma in Science and Technology of Digital and Computer Systems (STI2D) at Lycée Louis Armand in Paris. This program provided me with a solid foundation in computer science, programming, and digital systems, preparing me for further studies in the field.",
+        },
+        fr: {
+            Time: "2019",
+            Title: "NSI (Numérique et Sciences Informatiques), Lycée",
+            Period: "3 ans",
+            Description:
+                "Durant mes deux dernières années de lycée, j’ai choisi ce qui me semblait juste à ce moment-là, et je ne l’ai jamais regretté. J’ai opté pour les spécialités Mathématiques et NSI, deux choix qui m’ont vraiment ouvert les portes du monde de l’ingénierie. C’est à ce moment-là que j’ai réalisé mon premier projet de robotique : un petit Arduino capable de faire tourner un jeu de Snake. J’y ai aussi découvert mes premiers algorithmes en Python, ce qui a renforcé mon intérêt pour le développement et la résolution de problèmes par le code.",
+        },
+        Company: "La Martinière Monplaisir",
+        CompanyLink: "https://martiniere-monplaisir.ent.auvergnerhonealpes.fr",
+        Location: "Lyon, France",
+    },
+    {
+        id: 4,
+        en: {
+            Time: "2018",
+            Title: "My First Encounter with “Coding”",
+            Period: "When It All Started",
+            Description:
+                "Just before high school, I discovered the 'Inspect Element' tool in my browser. I played around with it, changing values and text on websites. It was fun showing my friends that I was the CEO of Microsoft (at least on screen). One day, I decided to build a page from scratch, a simple HTML page to thank my mom. It looked awful, but I was proud of it. I wanted to make it look better, so I started searching… and that’s when I discovered CSS. And from there, the journey really began, I took the helm of my own little Vogue Merry and set sail into the vast sea of code.",
+        },
+        fr: {
+            Time: "2018",
+            Title: "Ma Première Rencontre avec le “Code”",
+            Period: "Quand Tout a Commencé",
+            Description:
+                "Juste avant le lycée, j’ai découvert l’outil « Inspecter l’élément » dans mon navigateur. Je m’amusais à modifier des textes et des valeurs sur les sites, et je faisais croire à mes amis que j’étais 'le PDG de Microsoft' (bon, au moins sur l'écran ! ). Un jour, j’ai voulu créer une page moi-même. J’ai donc fait une page HTML toute simple pour dire merci à ma mère. C’était affreux visuellement, mais j’en étais fier. J’ai voulu l’améliorer, alors j’ai cherché… et c’est comme ça que j’ai découvert CSS. Et depuis, j’ai embarqué à bord du Vogue Merry et pris la mer vers l’univers du code.",
+        },
+        Company: "",
+        CompanyLink: "",
+        Location: "",
+    },
+];
---
+++ src/data/ProjectData.tsx
@@ -0,0 +1,50 @@
+export default [
+    {
+        id: 1,
+        Title: "SteamList",
+        en: {
+            Description:
+                "A project that fetches all the games from your Steam library using your ID and lets you create a tier list with them (you can choose to exclude free games).",
+        },
+        fr: {
+            Description:
+                "Un projet qui récupère tous les jeux de votre bibliothèque Steam via votre ID et vous permet de créer une tier list avec ceux-ci (vous pouvez désactiver l’affichage des jeux gratuits).",
+        },
+        Thumbnail: "/src/assets/SteamListThumbnail.png",
+        Github: "https://github.com/FaroukMBen/SteamTierList",
+        Demo: "",
+        Tags: ["React", "TypeScript", "CSS"],
+    },
+    {
+        id: 2,
+        Title: "5Minigames",
+        en: {
+            Description:
+                "As the name suggests, a React project featuring five small minigames I built to practice this technology ahead of my next big project.",
+        },
+        fr: {
+            Description:
+                "Comme son nom l’indique, un projet React composé de cinq petits mini-jeux, conçu pour me former à cette technologie en vue de mon prochain gros projet.",
+        },
+        Thumbnail: "/src/assets/5MinigamesThumbnail.png",
+        Github: "https://github.com/FaroukMBen/5Minigames",
+        Demo: "",
+        Tags: ["React", "TypeScript", "CSS"],
+    },
+    {
+        id: 3,
+        Title: "MinimalPortfolio",
+        en: {
+            Description:
+                "A minimal portfolio created simply to showcase my projects without any fancy extras… and also to experiment with Tailwind CSS :)",
+        },
+        fr: {
+            Description:
+                "Un portfolio minimaliste, simplement conçu pour présenter mes projets de façon simple, sans extras… et également pour expérimenter Tailwind CSS :)",
+        },
+        Thumbnail: "",
+        Github: "https://github.com/FaroukMBen/MinimalPortfolio",
+        Demo: "",
+        Tags: ["React", "Tailwind CSS", "TypeScript"],
+    },
+];
