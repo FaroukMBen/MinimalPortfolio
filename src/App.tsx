c8aef70defffb450ff1006a641843b70fbe3efd1
FaroukMBen <FaroukMohamed.Bendeddouche@outlook.com>
2025-06-02 17:49:10 +0200

    chore(app): scaffold and import components into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Introduction from "./components/Introduction";
+import Portfolio from "./components/Portoflio";
+import Timeline from "./components/Timeline";
+import { UIProvider } from "./context/UIProvider";
+
 function App() {
-    return <></>;
+    return (
+        <UIProvider>
+            <Header />
+            <Introduction />
+            <Portfolio />
+            <Timeline />
+            <Contact />
+            <Footer />
+        </UIProvider>
+    );
 }
 
 export default App;
---
+++ src/components/Timeline.tsx
@@ -0,0 +1,4 @@
+function Timeline() {
+    return <div></div>;
+}
+export default Timeline;
