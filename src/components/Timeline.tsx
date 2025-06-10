89b2a99b86a89c6cb421e69595c3f2437544320f
FaroukMBen <FaroukMohamed.Bendeddouche@outlook.com>
2025-06-10 17:24:50 +0200

    feat(Timeline): use Experience component to render experiences from experienceData

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,4 +1,25 @@
+import { useContext } from "react";
+import { UIContext } from "../context/UIContext";
+import Experience from "./Experience";
+import experience from "../data/ExperienceData";
+
 function Timeline() {
-    return <div></div>;
+    const { uiValues } = useContext(UIContext);
+
+    return (
+        <div>
+            {experience.map((exp) => {
+                return Experience(
+                    exp[uiValues.language].Time,
+                    exp[uiValues.language].Title,
+                    exp[uiValues.language].Period,
+                    exp[uiValues.language].Description,
+                    exp.Company,
+                    exp.CompanyLink,
+                    exp.Location
+                );
+            })}
+        </div>
+    );
 }
 export default Timeline;
