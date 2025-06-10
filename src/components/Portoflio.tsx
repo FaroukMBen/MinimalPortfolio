6e72f4bf5d5e3b0e3e28d1c30348963ceb73c30b
FaroukMBen <FaroukMohamed.Bendeddouche@outlook.com>
2025-06-10 17:10:53 +0200

    feat(Portfolio): use Project component to render projects from projectData

--- src/components/Portoflio.tsx
+++ src/components/Portoflio.tsx
@@ -1,4 +1,23 @@
+import { useContext } from "react";
+import projects from "../data/ProjectData";
+import Project from "./Project";
+import { UIContext } from "../context/UIContext";
+
 function Portfolio() {
-    return <div></div>;
+    const { uiValues } = useContext(UIContext);
+
+    return (
+        <div>
+            {projects.map((project) => {
+                return Project(
+                    project.Title,
+                    project[uiValues.language].Description,
+                    project.Thumbnail,
+                    project.Github,
+                    project.Tags
+                );
+            })}
+        </div>
+    );
 }
 export default Portfolio;
