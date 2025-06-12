ea44cf8235346a8ca2fd76d163301de20e2e1747
FaroukMBen <FaroukMohamed.Bendeddouche@outlook.com>
2025-06-12 11:09:11 +0200

    feat(header): allow user to change theme and language

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,34 @@
+import { useContext } from "react";
+import { UIContext } from "../context/UIContext";
+
 function Header() {
-    return <div></div>;
+    const { uiValues, setUIValues } = useContext(UIContext);
+
+    const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setUIValues({
+            ...uiValues,
+            language: e.target.value === "fr" ? "fr" : "en",
+        });
+    };
+
+    return (
+        <header>
+            <img src="images/react.svg" alt="logo" />
+            <select onChange={handleLanguageChange} value={uiValues.language}>
+                <option value="fr">Français</option>
+                <option value="en">English</option>
+            </select>
+            <button
+                onClick={() =>
+                    setUIValues({
+                        ...uiValues,
+                        theme: uiValues.theme === "light" ? "dark" : "light",
+                    })
+                }
+            >
+                {uiValues.theme}
+            </button>
+        </header>
+    );
 }
 export default Header;
